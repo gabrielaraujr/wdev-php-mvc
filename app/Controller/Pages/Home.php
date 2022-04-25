a26f9f81a919dc40e2a97f9270178d7c5a2e0ce1
Gabriel Araujo <matadoxx@gmail.com>
2022-04-25 19:16:41 -0300

    chore: first contact with controller and view

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use \App\Utils\View;
+use \App\Model\Entity\Organization;
+
+class Home extends Page {
+  
+  /**
+   * Método responsável por retornar o conteúdo (view) da nossa página genérica
+   * @return string
+   */
+  public static function getHome() {
+    $content = View::render('pages/home', [
+      'name' => 'WDEV - Canal',
+      'description' => 'Canal do youtube: https://youtube.com.br/wdevoficial',
+      'site' => 'https://youtube.com.br/wdevoficial'
+    ]);
+
+    return parent::getPage('W DEV - Canal - HOME', $content);
+  }
+
+}
